3537a37df24ed5e1851d36d336f20549e76a645b
Cong <erdongerzong@qq.com>
2017-10-25 23:45:49 +0100

    cw1 2b

--- mlp/initialisers.py
+++ mlp/initialisers.py
@@ -65,10 +65,12 @@ class NormalInit(object):
         return self.rng.normal(loc=self.mean, scale=self.std, size=shape)
 
 class SELUInit(object):
-    """SELU Initializer."""
+    """SELU Initializer. 
+    Initialising a SELU layer drawing from a Gaussian with mean 0 and variance 1/n out"""
     # write code that implements SELU initialization. Take inspiration from the other initializers in this file.
-    pass
+    return self.rng.normal(loc=0, scale=(1/shape[1])**0.5, size=shape)
 
+    
 class GlorotUniformInit(object):
     """Glorot and Bengio (2010) random uniform weights initialiser.
     Initialises an two-dimensional parameter array using the 'normalized
@@ -138,3 +140,59 @@ class GlorotNormalInit(object):
     def __call__(self, shape):
         std = self.gain * (2. / (shape[0] + shape[1]))**0.5
         return self.rng.normal(loc=0., scale=std, size=shape)
+
+
+        
+ class Fan_in_UniformInit(object):
+    """Initialises an two-dimensional uniform parameter array.
+    Weights are sampled from a zero-mean uniform distribution with standard
+    deviation `sqrt(1 / (input_dim))` where `input_dim` and
+    `output_dim` are the input and output dimensions of the weight matrix
+    respectively."""
+    def __init__(self, gain=1., rng=None):
+        """Construct a normalised initilisation random initialiser object.
+        Args:
+            gain: Multiplicative factor to scale initialised weights by.
+                Recommended values is 1 for affine layers followed by
+                logistic sigmoid layers (or another affine layer).
+            rng (RandomState): Seeded random number generator.
+        """
+        self.gain = gain
+        if rng is None:
+            rng = np.random.RandomState(DEFAULT_SEED)
+        self.rng = rng
+    
+    def __call__(self, shape):
+        assert len(shape) == 2, (
+            'Initialiser should only be used for two dimensional arrays.')
+        std = self.gain * (1. / shape[0])**0.5
+        half_width = 3.**0.5 * std
+        return self.rng.uniform(low=-half_width, high=half_width, size=shape)
+
+       
+ class Fan_out_UniformInit(object):
+    """Initialises an two-dimensional uniform parameter array.
+    Weights are sampled from a zero-mean uniform distribution with standard
+    deviation `sqrt(1 / (output_dim))` where `input_dim` and
+    `output_dim` are the input and output dimensions of the weight matrix
+    respectively."""
+    def __init__(self, gain=1., rng=None):
+        """Construct a normalised initilisation random initialiser object.
+        Args:
+            gain: Multiplicative factor to scale initialised weights by.
+                Recommended values is 1 for affine layers followed by
+                logistic sigmoid layers (or another affine layer).
+            rng (RandomState): Seeded random number generator.
+        """
+        self.gain = gain
+        if rng is None:
+            rng = np.random.RandomState(DEFAULT_SEED)
+        self.rng = rng
+    
+    def __call__(self, shape):
+        assert len(shape) == 2, (
+            'Initialiser should only be used for two dimensional arrays.')
+        std = self.gain * (1. / shape[1])**0.5
+        half_width = 3.**0.5 * std
+        return self.rng.uniform(low=-half_width, high=half_width, size=shape)
+       
